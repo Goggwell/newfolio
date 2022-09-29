012005c790c19826d59d90862c6b7959ddea301d
Samuel Yusuf <zulkarnaensamuel1@gmail.com>
2022-09-29 16:05:22 +0700

    feat: adding styles to Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,20 +4,37 @@ interface IButtonProps {
   onClick?: MouseEventHandler<HTMLButtonElement>
   title?: string
   children?: ReactNode | JSX.Element
+  backgroundColor?: string
+  padding?: number
 }
 
 /**
  * Standard button component
  * @function Button
  * @param {MouseEventHandler<HTMLButtonElement>?} onClick - handler for button click events
- * @param {string?} title - title text of button
+ * @param {string} title - title text of button (must put)
  * @param {ReactNode | JSX.Element?} children - HTML children to render
+ * @param {string?} backgroundColor - background color of button
+ * @param {number?} padding - space within the button and around the button's content
  * @returns {JSX.Element} - Rendered component
  */
 
-const Button = ({ onClick, title, children }: IButtonProps) => {
+const Button = ({
+  onClick,
+  title = 'Default',
+  children,
+  backgroundColor,
+  padding = 4,
+}: IButtonProps) => {
   return (
-    <button data-testid="button" onClick={onClick}>
+    <button
+      data-testid="button"
+      onClick={onClick}
+      style={{
+        backgroundColor,
+        padding: `${padding}px`,
+      }}
+    >
       {title}
       {children}
     </button>
