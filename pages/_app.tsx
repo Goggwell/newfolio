589a3da9b8d4072b33753b7fa05560b71075ae28
Samuel Yusuf <zulkarnaensamuel1@gmail.com>
2022-10-03 02:43:00 +0700

    chore: adding ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      themes={[
+        'dark',
+        'coquelicot',
+        'vice',
+        'nostalgia',
+        'forest',
+        'lab',
+        'zomp',
+      ]}
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
