3f5fdebfb8cebee732ca0ad496d4c3676826a0fe
Samuel Yusuf <zulkarnaensamuel1@gmail.com>
2022-10-03 04:09:46 +0700

    fix: svg typing

--- globals.d.ts
+++ globals.d.ts
@@ -1,6 +1,6 @@
 declare module '*.svg' {
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
-  const content: string
+  const content: string | any
 
   export { ReactComponent }
   export default content
