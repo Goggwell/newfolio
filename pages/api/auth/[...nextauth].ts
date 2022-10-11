23ddaccab5938ead6b1f5c35ecbacd2cec212c0b
Samuel Yusuf <zulkarnaensamuel1@gmail.com>
2022-10-11 23:30:12 +0700

    fix(next-auth): removing Discord provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,14 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth'
-import DiscordProvider from 'next-auth/providers/discord'
+import NextAuth, { type NextAuthOptions, User as NextAuthUser } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from '@/server/db/client'
 import { env } from '@/env/server.mjs'
+
+interface NextAuthUserWithStringId extends NextAuthUser {
+  id: string
+}
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -20,13 +23,17 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     GithubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        } as NextAuthUserWithStringId
+      },
     }),
     // ...add more providers here
   ],
