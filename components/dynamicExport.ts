ca8bf450aea24e90aa63358971eea1d8c5cf300c
Samuel Yusuf <zulkarnaensamuel1@gmail.com>
2022-10-12 00:19:05 +0700

    chore(dynamicExport): enabling ssr for dynamic components

--- components/dynamicExport.ts
+++ components/dynamicExport.ts
@@ -1,56 +1,31 @@
 import dynamic from 'next/dynamic'
 
 export const DynamicProgram = dynamic(
-  () => import('@/components/Program/Program'),
-  {
-    ssr: false,
-  }
+  () => import('@/components/Program/Program')
 )
 export const DynamicJournal = dynamic(
-  () => import('@/components/Programs/Journal/JournalProgram/JournalProgram'),
-  {
-    ssr: false,
-  }
+  () => import('@/components/Programs/Journal/JournalProgram/JournalProgram')
 )
 export const DynamicTheme = dynamic(
-  () => import('@/components/Programs/Theme/ThemeProgram/ThemeProgram'),
-  {
-    ssr: false,
-  }
+  () => import('@/components/Programs/Theme/ThemeProgram/ThemeProgram')
 )
 export const DynamicFeed = dynamic(
-  () => import('@/components/Programs/Feed/FeedProgram/FeedProgram'),
-  {
-    ssr: false,
-  }
+  () => import('@/components/Programs/Feed/FeedProgram/FeedProgram')
 )
 export const DynamicProjects = dynamic(
-  () =>
-    import('@/components/Programs/Projects/ProjectsProgram/ProjectsProgram'),
-  {
-    ssr: false,
-  }
+  () => import('@/components/Programs/Projects/ProjectsProgram/ProjectsProgram')
 )
 export const DynamicAbout = dynamic(
-  () => import('@/components/Programs/About/AboutProgram/AboutProgram'),
-  {
-    ssr: false,
-  }
+  () => import('@/components/Programs/About/AboutProgram/AboutProgram')
 )
 export const DynamicExperience = dynamic(
   () =>
     import(
       '@/components/Programs/Experience/ExperienceProgram/ExperienceProgram'
-    ),
-  {
-    ssr: false,
-  }
+    )
 )
 export const DynamicContact = dynamic(
-  () => import('@/components/Programs/Contact/ContactProgram/ContactProgram'),
-  {
-    ssr: false,
-  }
+  () => import('@/components/Programs/Contact/ContactProgram/ContactProgram')
 )
 
 export const DynamicClock = dynamic(() => import('@/components/Clock/Clock'), {
