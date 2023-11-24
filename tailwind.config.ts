3b808ad035990126e2c7ebf3c08655ddbb83361d
Goggwell <zulkarnaensamuel1@gmail.com>
2023-11-25 05:27:03 +0700

    feat: theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        backgroundRGB: "hsl(var(--backgroundRGB))",
+        text: "hsl(var(--text))",
+        dot: "hsl(var(--dot))",
+        programOutine: "hsl(var(--programOutline))",
+        program: "hsl(var(--program))",
+        select: "hsl(var(--select))",
+        themeTitle: "hsl(var(--themeTitle))",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
