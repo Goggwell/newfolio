34968e0f85532a2e180e07eaf3737d9cdf410b43
samuel-bd <samuely@frisseblikken.com>
2024-08-01 13:26:11 +0700

    chore: migrate to tailwind v4
    Significantly smaller and faster, will need to figure out how to set up multiple themes but for now we can stick with light and dark

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 /// <reference types="vitest" />
+import tailwindcss from "@tailwindcss/vite";
 // import MillionLint from "@million/lint";
 import react from "@vitejs/plugin-react-swc";
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-const _plugins = [react()];
+const _plugins = [react(), tailwindcss()];
 export default defineConfig({
 	plugins: _plugins,
 	resolve: {
@@ -16,5 +19,14 @@ export default defineConfig({
 		environment: "jsdom",
 		globals: true,
 		setupFiles: "./src/tests/setup.ts",
+	},
+	css: {
+		transformer: "lightningcss",
+		lightningcss: {
+			targets: browserslistToTargets(browserslist(">= 0.25%")),
+		},
+	},
+	build: {
+		cssMinify: "lightningcss",
 	},
 });
